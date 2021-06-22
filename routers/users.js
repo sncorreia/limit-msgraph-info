6ff6653adac3c3d7252d9e55d04631f36657cf4d
Sérgio Correia <secorre@microsoft.com>
2021-06-22 22:53:56 +0100

    Implementation of authService and fetchService

--- routers/users.js
+++ routers/users.js
@@ -1,19 +1,29 @@
 const express = require("express");
+const auth = require("../services/authService");
 
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-    res.status(200).json({
-        "status": "Endpoint Alive",
-        "info": "It will return all the users with limited info from MS Graph"
-    })
+router.get("/users", async (req, res) => {
+    try {
+        const authResponse = await auth.getToken(auth.tokenRequest);
+        res.status(200).json({
+            "status": "Endpoint Alive",
+            "info": "It will return all the users with limited info from MS Graph",
+            "access_token": authResponse.accessToken
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
-router.get("/users/:id", (req, res) => {
+router.get("/users/:id", async (req, res) => {
     const id = req.params.id;
+    const authResponse = await auth.getToken(auth.tokenRequest);
     res.status(200).json({
         "status": "Endpoint Alive",
-        "info": `It will return the user ${id} with limited info from MS Graph`
+        "info": `It will return the user ${id} with limited info from MS Graph`,
+        "access_token": authResponse.accessToken
     })
 });
 
