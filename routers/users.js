c400f195969eab59c9d9697ede1b6b2b531a6205
Sérgio Correia <secorre@microsoft.com>
2021-06-22 22:55:15 +0100

    Refactor routers with try-catch

--- routers/users.js
+++ routers/users.js
@@ -19,12 +19,16 @@ router.get("/users", async (req, res) => {
 
 router.get("/users/:id", async (req, res) => {
     const id = req.params.id;
-    const authResponse = await auth.getToken(auth.tokenRequest);
-    res.status(200).json({
-        "status": "Endpoint Alive",
-        "info": `It will return the user ${id} with limited info from MS Graph`,
-        "access_token": authResponse.accessToken
-    })
+    try {
+        const authResponse = await auth.getToken(auth.tokenRequest);
+        res.status(200).json({
+            "status": "Endpoint Alive",
+            "info": `It will return the user ${id} with limited info from MS Graph`,
+            "access_token": authResponse.accessToken
+        })
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
